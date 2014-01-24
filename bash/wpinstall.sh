3952b8d98691bb4e990912336262a2870a418379
jooiiee <hej@jooiiee.se>
2014-01-24 23:02:13 +0100

    updated the wpinstall script with better comments

--- bash/wpinstall.sh
+++ bash/wpinstall.sh
@@ -14,51 +14,53 @@ then
 fi
 
 # Download, install and configure LAMP 
-
 yum install -y httpd mysql mysql-server php
-
 chconfig mysqld on
 chconfig httpd on
-
 service mysqld start
 service httpd start
 
-
+# Configure mysql
 echo "Configure mysql with a secure password and answer yes to the other questions"
 read -s -n 1 -p "Press any key to continue" none
 echo ""
 
+# The secure installation script could be replaced, but that would be a hassle. This is quick n dirty. 
 mysql_secure_installation
 
 # Download wordpress
-
 curl http://wordpress.org/latest.tar.gz >  /tmp/wordpress.tar.gz
-
 tar -xzf /tmp/wordpress.tar.gz -C /var/www/
 mv /var/www/wordpress /var/www/html
 
+# Adjust file permissions
+chmod -R 760 /var/www/http 
+chown -R www-data:www-data
+adduser root www-data
 
-# Set up sql
+# Set up sql for wordpress
+	# Generate password
+	mysqlpassword=$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)
 
-mysqlpassword=$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)
+	# Write command file
+	echo "CREATE USER \"wordpress\"@\"localhost\" IDENTIFIED BY '$(echo $mysqlpassword)';" > /tmp/setup.sql
+	echo 'GRANT ALL ON wordpress.* TO "wordpress"@"localhost";' >> /tmp/setup.sql
+	echo "FLUSH PRIVILEGES;" >> /tmp/setup.sql
 
-echo "CREATE USER \"wordpress\"@\"localhost\" IDENTIFIED BY '$(echo $mysqlpassword)';" > /tmp/setup.sql
-echo 'GRANT ALL ON wordpress.* TO "wordpress"@"localhost";' >> /tmp/setup.sql
-echo "FLUSH PRIVILEGES;" >> /tmp/setup.sql
+	# Execute comand file
+	echo "The next prompt is for the mysql password you set earlier"
+	mysql -u root -p < /tmp/setup.sql
 
-echo "The next prompt is for the mysql password you set earlier"
-mysql -u root -p < /tmp/setup.sql
+	# Remove command file
+	# The command file contains the sql password in clear text
+	rm /tmp/setup.sql
 
-rm /tmp/setup.sql
-
-echo 'The database for wordpress is "wordpress"'
-echo "The password for wordpress is \"$mysqlpassword\""
-echo "The password is availible in the file /tmp/password"
-echo "Dont forget to remove the password file"
-echo $mysqlpassword > /tmp/password
-chmod 600 /tmp/password
+	# Echo sql info to user
+	echo 'The database for wordpress is "wordpress"'
+	echo "The password for wordpress is \"$mysqlpassword\""
 
 
-echo "Open a browser and run the installer"
+echo "If no errors were reported, wordpress core should be installed and ready for configuration"
+echo "Continue the install from a browser"
 
-exit
+exit 0
