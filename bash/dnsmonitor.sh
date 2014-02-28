28694797a91ea645795ebbf7ad85b9e7f95d0cbd
jooiiee <hej@jooiiee.se>
2014-02-28 21:27:44 +0100

    Added DNSmonitor

---
+++ bash/dnsmonitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# I got tired of waiting for DNS propogation, so I wrote this quick script to monitor it and use the notify-send function to tell me about it. 
+# There is no syntax checking, run it with the domain as argument 1 and the new ip as argument 2. 
+
+	while true
+	do
+
+		ip=$(host -t a $1 | cut -f 4 -d ' ')
+
+		if [ $ip == 195.74.38.155 ]
+
+		then 
+			notify-send "DNS change done" "The DNS change is now complete, $1 now points to $2"
+			play /usr/share/sounds/ubuntu/stereo/dialog-question.ogg
+			exit 0
+		fi
+	done
+
+exit 1
